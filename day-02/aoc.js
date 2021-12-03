41470b53ac9bb3e68295371e83365d5a7974ef40
Matt Machuga <machuga@gmail.com>
2021-12-02 21:59:04 -0500

    Day 02 Part 2

--- day-02/aoc.js
+++ day-02/aoc.js
@@ -1,39 +1,54 @@
 const { parseInput, pipe, map, reduce } = require('../util');
 const [filename = 0] = process.argv.slice(2);
-const directionTable = {
-  'forward': ['h', (value) => value],
-  'down': ['d', (value) => value],
-  'up': ['d', (value) => -value],
-};
-
-const calculateDirection = ([direction, value]) => {
-  const [dir, fn] = directionTable[direction];
-
-  return [dir, fn(parseInt(value, 10))];
-};
 
 const getInstructions = pipe([
   parseInput,
   map((line) => line.split(' ')),
-  map(calculateDirection),
+  map(([direction, amount]) => [direction, parseInt(amount, 10)]),
 ]);
-
-const calculate = pipe([
-  getInstructions,
-  reduce((acc, [direction, amount]) => {
-    acc[direction] += amount;
-
-    return acc;
-  }, { h: 0, d: 0 }),
-  ({ h, d }) => h * d,
-]);
-
 
 const part1 = () => {
+  const directionTable = {
+    'forward': ['h', (value) => value],
+    'down': ['d', (value) => value],
+    'up': ['d', (value) => -value],
+  };
+
+  const calculateDirection = ([direction, value]) => {
+    const [dir, fn] = directionTable[direction];
+
+    return [dir, fn(parseInt(value, 10))];
+  };
+
+  const calculate = pipe([
+    getInstructions,
+    map(calculateDirection),
+    reduce((acc, [direction, amount]) => {
+      acc[direction] += amount;
+
+      return acc;
+    }, { h: 0, d: 0 }),
+    ({ h, d }) => h * d,
+  ]);
+
+
   console.log(`Part 1: Total is "${calculate(filename)}"`);
 };
 
 const part2 = () => {
+  const calculate = pipe([
+    getInstructions,
+    reduce(({ h, d, aim }, [direction, amount]) => {
+      switch (direction) {
+        case 'up': return { h, d, aim: aim - amount };
+        case 'down': return { h, d, aim: aim + amount };
+        case 'forward': return { h: h + amount, d: d + (aim * amount), aim };
+        default: throw new Error('Something went horribly wrong');
+      }
+    }, { h: 0, d: 0, aim: 0 }),
+    ({ h, d }) => h * d,
+  ]);
+  console.log(`Part 2: Total is "${calculate(filename)}"`);
 };
 
 
