a21feb297ff24b27d18fd90961271ced41496561
Matt Machuga <machuga@gmail.com>
2021-12-03 23:31:31 -0500

    Adding transpose and tap helpers

--- util.js
+++ util.js
@@ -8,3 +8,10 @@ module.exports.pipe = (fns) => (x) => fns.reduce((v, f) => f(v), x);
 module.exports.map = (fn) => (arr) => arr.map(fn);
 
 module.exports.reduce = (fn, initial = []) => (arr) => arr.reduce(fn, initial);
+
+module.exports.transpose = (array) => array[0].map((_, colIndex) => array.map(row => row[colIndex]));
+
+module.exports.tap = (str) => (value) => {
+  console.log(str, value);
+  return value;
+};
